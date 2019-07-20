6d99b0b84359c3c832b6618e300ddc651c0d584c
Bruno Santos <bruno.js02@gmail.com>
2019-07-20 17:17:21 -0300

    Updated gitignore to ignore config file

--- src/screens/home/components/popular-movies/PopularMovieItem.js
+++ src/screens/home/components/popular-movies/PopularMovieItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Image, View, TouchableOpacity } from 'react-native';
 import { withNavigation } from 'react-navigation';
+import { Image, View, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 import { Text } from '~/components';
 import { resourceBaseUrl } from '~/../env.json';
 
 const propTypes = {
   movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     poster_path: PropTypes.string.isRequired,
     release_date: PropTypes.string.isRequired,
@@ -25,8 +26,8 @@ function PopularMovieItem({ movie, navigation: { navigate } }) {
     <View style={{ width: 130 }}>
       <TouchableOpacity onPress={() => navigate('movie', { id })}>
         <Image
-          style={{ width: 120, height: 170 }}
-          source={{ uri: `${resourceBaseUrl}w185${pic}` }}
+          style={{ width: 120, height: 170, borderRadius: 5 }}
+          source={{ uri: `${resourceBaseUrl}w342${pic}` }}
           resizeMode="stretch"
         />
         <View style={{ paddingHorizontal: 5, alignItems: 'center', marginTop: 8 }}>
