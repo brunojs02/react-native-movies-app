0d5a378a34bd819c4530c47f71e4734f06664152
Bruno Santos <bruno.js02@gmail.com>
2019-10-14 23:05:17 -0300

    Provided is no longer necessary

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import { Provider } from 'react-redux';
 import store from './store';
 import Nav from './navigation';
 import { StatusBar } from './components';
-import ThemeContextProvider from './theme/provider';
 
 const App = () => (
   <Provider store={store}>
     <StatusBar />
-    <ThemeContextProvider>
-      <Nav />
-    </ThemeContextProvider>
+    <Nav />
   </Provider>
 );
 
